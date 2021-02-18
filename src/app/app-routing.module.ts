0c0feff3f005ebec607bcccac0ae77f431472ec0
Dmitriy Burkovskiy <dmitriy.burkovskiy@powercode.us>
2021-02-18 10:39:07 +0200

    Add main page
    Add header component
    Add navigation between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommitsComponent } from './pages/commits/commits.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'commits', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'commits', component: CommitsComponent }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GithubService } from './services/github.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
