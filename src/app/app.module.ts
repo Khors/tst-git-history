cd80bf46eddceec185ade3cf224e1ea265de7122
Dmitriy Burkovskiy <dmitriy.burkovskiy@powercode.us>
2021-02-18 09:38:20 +0200

    Fix: add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { GithubService } from './services/github.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [
     GithubService
